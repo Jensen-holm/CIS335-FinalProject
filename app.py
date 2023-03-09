2f74dd31aa80666c9e1180e01db7c5bd363eacf4
Jensen-holm <holmj@mail.gvsu.edu>
2023-03-09 11:47:16 -0600

    got the webcam running in main function

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import cv2
 
 
 def main():
-    return
+    st.title("Webcam Live Feed")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+
+    video_capture = cv2.VideoCapture(0)
+
+    while run:
+        _, frame = video_capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+
+    video_capture.release()
 
 
 if __name__ == "__main__":
